18418af3fd6251cd4ee6672f974c5e214f46d919
Andry Kamamy <tchithierry@gmail.com>
2023-10-14 13:25:15 +0300

    parametrage de l'affichage des produit par le filtre

--- src/pages/Eshop/Catalogue/Productlist/Productlist.jsx
+++ src/pages/Eshop/Catalogue/Productlist/Productlist.jsx
@@ -2,21 +2,27 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export const Productlist = ({ proucts, ShowCateg }) => {
- return (
-  <>
-   <h1>Productlist</h1>
-   <div>
-    <Link to="Panier">Panier</Link>
-   </div>
-   <div>
-    {proucts.filter(product => ShowCateg[product.category]).map((product) => (
-     <div key={product.id}>
-      <h2>{product.name}</h2>
-      <p>{product.price}</p>
-      <p>{product.stocked ? 'En stock' : 'En rupture de stock'}</p>
-     </div>
-    ))}
-   </div>
-  </>
- );
+  return (
+    <>
+      <h1>Productlist</h1>
+      <div>
+        <Link to="Panier">Panier</Link>
+      </div>
+      <div>
+        {ShowCateg ? proucts.filter(product => ShowCateg[product.category]).map((product) => (
+          <div key={product.id}>
+            <h2>{product.name}</h2>
+            <p>{product.price}</p>
+            <p>{product.stocked ? 'En stock' : 'En rupture de stock'}</p>
+          </div>
+        )) : proucts.map((product) => (
+          <div key={product.id}>
+            <h2>{product.name}</h2>
+            <p>{product.price}</p>
+            <p>{product.stocked ? 'En stock' : 'En rupture de stock'}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 };
