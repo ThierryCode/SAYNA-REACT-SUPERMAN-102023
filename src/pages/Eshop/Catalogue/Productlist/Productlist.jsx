311e9eece1c1c4a29e4393e396f4e341e32515a7
ThierryCode <tchithierry@gmail.com>
2023-10-17 18:24:11 +0300

    Ajout des images de produits

--- src/pages/Eshop/Catalogue/Productlist/Productlist.jsx
+++ src/pages/Eshop/Catalogue/Productlist/Productlist.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { PRODUCTS } from '../../../../products';
 import { shopContext } from '../../../../context/shopContext';
 // import '../../../../App.css';
@@ -17,6 +17,7 @@ export const Productlist = ({ShowCateg }) => {
       <div>
         {ShowCateg ? PRODUCTS.filter(PRODUCTS => ShowCateg[PRODUCTS.category]).map((PRODUCTS) => (
           <li className="Card col-3 ProductListli" key={PRODUCTS.id}>
+            {/* <img src={imageProduct} alt="" /> */}
             <div className="infoProduct">              
               <p className='productName text-center my-3'>{PRODUCTS.name}</p>
               <p className='productPrice'>{PRODUCTS.price}</p>
@@ -29,8 +30,9 @@ export const Productlist = ({ShowCateg }) => {
           </li>
         )) : PRODUCTS.map((PRODUCTS) => (
           <li className="Card col-3 ProductListli" key={PRODUCTS.id}>
-            <h2>{PRODUCTS.name}</h2>
-            <p>{PRODUCTS.price}</p>
+            {/* <img src={imageProduct} alt="" /> */}
+            <p className='productName'>{PRODUCTS.name}</p >
+            <p className='productPrice'>{PRODUCTS.price}</p>
             <div className="Button">
               <button onClick = {() => addToCart(PRODUCTS.id)}>
                 Ajouter au panier {cartItemAmount > 0 && <>({cartItemAmount})</>}
--- src/products.js
+++ src/products.js
@@ -1,14 +1,44 @@
+import product1 from './assets/imageProduct/autocollant.png'
+import product2 from './assets/imageProduct/autocolant.png'
+import product3 from './assets/imageProduct/cap.jpg'
+import product4 from './assets/imageProduct/comics.jpg'
+import product5 from './assets/imageProduct/costume_chien.jpg'
+import product6 from './assets/imageProduct/Costume.jpg'
+import product7 from './assets/imageProduct/Costume1.jpg'
+import product8 from './assets/imageProduct/Costume3.jpg'
+import product9 from './assets/imageProduct/JOUET.jpg'
+import product10 from './assets/imageProduct/jouet.png'
+import product11 from './assets/imageProduct/jouet1.jpg'
+import product12 from './assets/imageProduct/jouet3.jps'
+import product13 from './assets/imageProduct/lego.jpg'
+import product14 from './assets/imageProduct/mobile.jpg'
+import product15 from './assets/imageProduct/Mug.jpg'
+import product16 from './assets/imageProduct/Mug1.jpg'
+import product17 from './assets/imageProduct/portecle.jpg'
+import product18 from './assets/imageProduct/Poster.jpg'
+import product19 from './assets/imageProduct/Postergoku'
+import product20 from './assets/imageProduct/teshh.jpg'
+import product21 from './assets/imageProduct/teshh2.jpg'
 export const PRODUCTS = [  
-    {category:"Goodies", name:"Mug Voyage", price:25, stocked: true, id:1},
-    {category:"Goodies", name:"Mug Ouef", price:18, stocked: false, id:2},
-    {category:"Goodies", name:"Mug Noel", price:18, stocked: true, id:3},
-    {category:"Goodies", name:"Horloge Batman", price:8, stocked: true, id:4},
-    {category:"Goodies", name:"Horloge", price:7, stocked: true, id:5},
-    {category:"Goodies", name:"Figurine", price:10, stocked: true, id:6},
-    {category:"Affiche/Poster", name:"Poster Batman (2022)", price:19, stocked: true, id:7},
-    {category:"Affiche/Poster", name:"Poster", price:5, stocked: false, id:8},
-    {category:"Vétements", name:"Tee-shirt batmobile", price:25, stocked: true, id:9},
-    {category:"Vétements", name:"Sweat 'Why so serious'", price:32, stocked: true, id:10},
-    {category:"jouets", name:"Batmobile Légo", price:22, stocked: true, id:11},
-    {category:"jouets", name:"Jouet Batman", price:22, stocked: true, id:12}
+    {category:"Autocollant", name:"Autocollant", price:25, stocked: true, id:1, imageProduct: product1},
+    {category:"Goodies", name:"Mug Ouef", price:18, stocked: false, id:2, imageProduct: product2},
+    {category:"Goodies", name:"Mug Noel", price:18, stocked: true, id:3, imageProduct: product3},
+    {category:"Goodies", name:"Horloge Batman", price:8, stocked: true, id:4, imageProduct: product4},
+    {category:"Goodies", name:"Horloge", price:7, stocked: true, id:5, imageProduct: product5},
+    {category:"Goodies", name:"Figurine", price:10, stocked: true, id:6, imageProduct: product6},
+    {category:"Affiche/Poster", name:"Poster Batman (2022)", price:19, stocked: true, id:7, imageProduct: product7},
+    {category:"Affiche/Poster", name:"Poster", price:5, stocked: false, id:8, imageProduct: product8},
+    {category:"Vétements", name:"Tee-shirt batmobile", price:25, stocked: true, id:9, imageProduct: product9},
+    {category:"Vétements", name:"Sweat 'Why so serious'", price:32, stocked: true, id:10, imageProduct: product10},
+    {category:"jouets", name:"Batmobile Légo", price:22, stocked: true, id:11, imageProduct: product11},
+    {category:"jouets", name:"Jouet Batman", price:22, stocked: true, id:12, imageProduct: product12}
+    {category:"jouets", name:"Jouet Batman", price:22, stocked: true, id:12, imageProduct: product13}
+    {category:"jouets", name:"Jouet Batman", price:22, stocked: true, id:12, imageProduct: product14}
+    {category:"jouets", name:"Jouet Batman", price:22, stocked: true, id:12, imageProduct: product15}
+    {category:"jouets", name:"Jouet Batman", price:22, stocked: true, id:12, imageProduct: product16}
+    {category:"jouets", name:"Jouet Batman", price:22, stocked: true, id:12, imageProduct: product17}
+    {category:"jouets", name:"Jouet Batman", price:22, stocked: true, id:12, imageProduct: product18}
+    {category:"jouets", name:"Jouet Batman", price:22, stocked: true, id:12, imageProduct: product19}
+    {category:"jouets", name:"Jouet Batman", price:22, stocked: true, id:12, imageProduct: product20}
+    {category:"jouets", name:"Jouet Batman", price:22, stocked: true, id:12, imageProduct: product21}
     ]
